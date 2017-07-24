c4a63c5bfbda19359eb44dccb71182bacd07324b
Ray Lugo, Jr <rlugojr@gmail.com>
2017-07-24 15:37:20 -0400

    Syntax error
    Fixed missing commas in object instantiations.

--- movie_data.py
+++ movie_data.py
@@ -15,7 +15,7 @@ toy_story = media.Movie(
     "Toy Story",
     "1995",
     "A story of a boy and his toys that come to life.",
-    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg"
+    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
     "https://www.youtube.com/watch?v=vwyZH85NQC4"
     )
 
@@ -23,7 +23,7 @@ avatar = media.Movie(
     "Avatar",
     "2009",
     "A Marine on an alien planet",
-    "https://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg"
+    "https://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
     "https://www.youtube.com/watch?v=d1_JBMrrYw8"
     )
 
@@ -31,7 +31,7 @@ die_hard = media.Movie(
     "Die hard",
     "1988",
     "40 Stories Of Sheer Adventure!",
-    "https://upload.wikimedia.org/wikipedia/en/7/7e/Die_hard.jpg"
+    "https://upload.wikimedia.org/wikipedia/en/7/7e/Die_hard.jpg",
     "https://www.youtube.com/watch?v=QIOX44m8ktc"
     )
 
@@ -39,7 +39,7 @@ logan = media.Movie(
     "Logan",
     "2017",
     "His time has come.",
-    "https://upload.wikimedia.org/wikipedia/en/3/37/Logan_2017_poster.jpg"
+    "https://upload.wikimedia.org/wikipedia/en/3/37/Logan_2017_poster.jpg",
     "https://www.youtube.com/watch?v=Div0iP65aZo"
 )
 
@@ -47,7 +47,7 @@ true_romance = media.Movie(
     "True Romance",
     "1993",
     "Stealing, Cheating, Killing. Who said romance is dead?",
-    "https://upload.wikimedia.org/wikipedia/en/d/d6/True_romance.jpg"
+    "https://upload.wikimedia.org/wikipedia/en/d/d6/True_romance.jpg",
     "https://www.youtube.com/watch?v=_wNYNDzKpuQ"
 )
 
