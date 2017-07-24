344827ab9c229c63161bfe68f07a03cf2aae7b65
Ray Lugo, Jr <rlugojr@gmail.com>
2017-07-24 15:32:50 -0400

    Updated entertainment_center.py
    Updated entertainment_center.py to fix issues found by PEP8 linter.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,11 +1,11 @@
 import movie_data
 import fresh_tomatoes
 
-#retrieve movie list from movie_data.py.
-#this also creates instances of movie objects
-#and populates.
+'''retrieve movie list from movie_data.py.
+this also creates instances of movie objects
+and populates.'''
 movies = movie_data.movie_list
 
-#generate the web page using the movie_list
-#and displays it in the browser.
+'''generate the web page using the movie_list
+#and displays it in the browser.'''
 fresh_tomatoes.open_movies_page(movies)
