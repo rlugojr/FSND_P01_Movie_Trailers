f5f6467e98a57bbfeeb1deac93366fd83d653ad4
Ray Lugo, Jr <rlugojr@gmail.com>
2017-07-24 15:29:35 -0400

    Updated media.py
    Updated media.py to fix issues found by PEP8 linter.

--- media.py
+++ media.py
@@ -1,9 +1,21 @@
+
+'''The media.Movie Class provides a data structure to store
+ movie related information'''
+
+
 class Movie():
-    """This Class provides a data structure to store movie related information"""
 
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-
-    def __init__(self, movie_title, movie_year, movie_storyline, poster_image, trailer_youtube):
+    '''The media.Movie constructor is used to instantiate a movie object.
+        Inputs (required):
+            movie_title --> Title of the movie.
+            movie_year --> The year the movie was released.
+            movie_storyline --> Tagline or Storyline of the movie.
+            poster_image --> The url to the image file for the movie poster.
+            trailer_youtube --> The url to the YouTube video for
+                                the movie trailer.
+        Outputs: Movie Object'''
+    def __init__(self, movie_title, movie_year, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.year = movie_year
         self.storyline = movie_storyline
