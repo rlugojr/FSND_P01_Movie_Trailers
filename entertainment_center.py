03c07c8566303797411589dafe3f64c6abe23e88
Ray Lugo, Jr <rlugojr@gmail.com>
2017-07-24 02:12:51 -0400

    Commit current code
    Commit prior code in new file in preparation for additional
    functionality.

---
+++ entertainment_center.py
@@ -0,0 +1,7 @@
+import movie_data
+import fresh_tomatoes
+
+
+movies = movie_data.movie_list
+
+fresh_tomatoes.open_movies_page(movies)
---
+++ movie_data.py
@@ -0,0 +1,15 @@
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life.",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+avatar = media.Movie("Avatar","A Marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+
+die_hard = media.Movie("Die hard","40 Stories Of Sheer Adventure!",
+                       "https://upload.wikimedia.org/wikipedia/en/7/7e/Die_hard.jpg",
+                       "https://www.youtube.com/watch?v=QIOX44m8ktc" )
+
+movie_list = [toy_story, avatar, die_hard]
