8595c726f4c3a8e909ade637e059eb9dd1cc22fe
Ray Lugo, Jr <rlugojr@gmail.com>
2017-07-24 03:32:09 -0400

    Added Movie Year
    Added movie year to class and instances.
    Changed Class doc string.

--- media.py
+++ media.py
@@ -1,18 +1,11 @@
-import webbrowser
-
 class Movie():
-    """ This Class provide a way to store movie related information """
+    """This Class provides a data structure to store movie related information"""
 
     VALID_RATINGS = ["G","PG","PG-13","R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_year, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
+        self.year = movie_year
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
-
-    #def show_info():
-        #code
--- movie_data.py
+++ movie_data.py
@@ -1,14 +1,14 @@
 import media
 
-toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life.",
+toy_story = media.Movie("Toy Story","1995","A story of a boy and his toys that come to life.",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-avatar = media.Movie("Avatar","A Marine on an alien planet",
+avatar = media.Movie("Avatar","2009","A Marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 
-die_hard = media.Movie("Die hard","40 Stories Of Sheer Adventure!",
+die_hard = media.Movie("Die hard","1988","40 Stories Of Sheer Adventure!",
                        "https://upload.wikimedia.org/wikipedia/en/7/7e/Die_hard.jpg",
                        "https://www.youtube.com/watch?v=QIOX44m8ktc" )
 
